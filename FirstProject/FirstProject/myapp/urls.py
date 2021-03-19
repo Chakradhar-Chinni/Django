d6528b66ee909a334b6ec457c9bda26a05c16276
Chakradhar Chinni <chakradhar.chinni@gmail.com>
2021-03-19 11:34:53 +0530

    Removed FirstProject directory

--- FirstProject/FirstProject/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from myapp import views
-urlpatterns = [
-    path('',views.indexfunction,name="index"),
-    path('user',views.userfunction,name="user"),
-    path('user1',views.user1function,name="user1"),
-]
