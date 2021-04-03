dde9c09dc5bb98d67878d7a0398ae369652bda37
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-04-03 23:54:32 +0530

    Update settings.py

--- refproject/refproject/settings.py
+++ refproject/refproject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from django.db.backends.mysql.base import DatabaseWrapper
 DatabaseWrapper.data_types['DateTimeField'] = 'datetime'
-#the above line is not mandatory 
+#the above line is not mandatory [used in the lecture]
 import crispy_forms
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
