5871283f7bfed2b7b75fecd3f3be5e6376bb013b
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-02 23:05:31 +0530

    Update Integer choices in models.py

--- Integer choices in models.py
+++ Integer choices in models.py
@@ -3,8 +3,8 @@ class Card(models.Model):
     class Suit(models.IntegerChoices): 
         DIAMOND = 1
         SPADE = 2 
-        HEART = 3 
-        CLUB = 4
+        HEART = 3  
+        CLUB = 4 
 
     suit = models.IntegerField(choices=Suit.choices)
     
