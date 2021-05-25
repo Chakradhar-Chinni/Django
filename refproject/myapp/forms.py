deac2c60295f2decf1646a01483c39de419c9717
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-25 23:03:33 +0530

    Update forms.py

--- refproject/myapp/forms.py
+++ refproject/myapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import User
-#from .models import modelnames
+#from .models import modelnames 
  
 class RegistrationForm(forms.ModelForm):
     class Meta:   
