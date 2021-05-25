5d35de8cda3f0f11c05168726e06232a531ce018
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-25 23:04:20 +0530

    Update forms.py

--- refproject/myapp/forms.py
+++ refproject/myapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import User
 #from .models import modelnames  
   
-class RegistrationForm(forms.ModelForm): 
+class RegistrationForm(forms.ModelForm):  
     class Meta:     
         model = User  
         fields = "__all__"
