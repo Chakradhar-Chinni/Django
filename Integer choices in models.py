5a0d87f9acead2532ae07c4e343e015f569a4066
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-31 23:19:42 +0530

    Update Integer choices in models.py

--- Integer choices in models.py
+++ Integer choices in models.py
@@ -1,7 +1,7 @@
 class Card(models.Model): 
   
     class Suit(models.IntegerChoices):       
-        DIAMOND = 1   
+        DIAMOND = 1    
         SPADE = 2      
         HEART = 3       
         CLUB = 4      
