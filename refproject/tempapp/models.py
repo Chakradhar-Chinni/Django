bb52b6247ceb4cdd64b1443825fe52ccf9f82aa6
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-07 23:42:55 +0530

    Update models.py

--- refproject/tempapp/models.py
+++ refproject/tempapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Candidate(models.Model):
-    Product   = models.CharField(max_length=100,blank=False)
+    Product   = models.CharField(max_length=100,blank=False) 
     Logistics = models.CharField(max_length=100,blank=False)
     class Meta:
         db_table = "candidate_table"
