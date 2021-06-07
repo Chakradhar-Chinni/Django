d7d8080a63f3b2379e0998c2f774275710f704bd
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-07 23:42:45 +0530

    Update models.py

--- refproject/tempapp/models.py
+++ refproject/tempapp/models.py
@@ -7,8 +7,9 @@ class Candidate(models.Model):
     class Meta:
         db_table = "candidate_table"
         
-class Product(models.Model):
+class Product(models.Model): 
     brand = models.CharField(max_length=100,blank=False)
     origin = models.CharField(max_length=100,blank=False)
     class Meta:
         db_table = "product_table" 
+ 
