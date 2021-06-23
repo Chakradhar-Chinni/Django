1ed8b72548627492bce93b85eb0d6768265439fa
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-23 22:51:50 +0530

    Update admin.py

--- refproject/tempapp/admin.py
+++ refproject/tempapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin 
 from .models import Candidate,Product 
 # Register your models here.
-admin.site.register(Candidate)
+admin.site.register(Candidate) 
 admin.site.register(Product)   
     
  
