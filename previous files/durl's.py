2b4649375a62cd9b798204b3a4bd3217f46f5754
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-30 23:26:44 +0530

    Update durl's.py

--- previous files/durl's.py
+++ previous files/durl's.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include 
 
 urlpatterns = [  
-    path('', include('calc.urls')),      
+    path('', include('calc.urls')),       
     path('admin/', admin.site.urls),    
 ] 
        
