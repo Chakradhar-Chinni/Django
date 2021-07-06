eeaaa270f78254e5d35b7ced5effd970bb2b533a
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-07-06 14:23:35 +0530

    Update a1views.py

--- previous files/a1views.py
+++ previous files/a1views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 # Create your views here.  
   
 def home(request):    
-    return HttpResponse("Hello World!! Welcome to Django!!");      
+    return HttpResponse("Hello World!! Welcome to Django!!")
               
-return render(request, 'home.html')        
+return render(request, 'home.html')         
      
     return render(request, 'home.html',{'name':'Chakradhar Chinni'})   
                                              
