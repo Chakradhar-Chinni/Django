a40f9697431917794b0d25757451c74462806c28
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-07-22 22:20:50 +0530

    Update Integer choices in models.py

--- Integer choices in models.py
+++ Integer choices in models.py
@@ -9,6 +9,6 @@ class Card(models.Model):
     suit = models.IntegerField(choices=Suit.choices)   
       
     Source(official documentation): https://docs.djangoproject.com/en/dev/ref/models/fields/#integerfield          
-               
+                
  
  
