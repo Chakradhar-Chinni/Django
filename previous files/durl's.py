adcdd0407549a14e46bf45c2c10047a7ea164755
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-07-29 23:30:33 +0530

    Update durl's.py

--- previous files/durl's.py
+++ previous files/durl's.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [     
     path('', include('calc.urls')),                        
     path('admin/', admin.site.urls),                                   
-]                    
+]                      
                                   
                             
                
